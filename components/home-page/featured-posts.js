2de40ad84e6b0ebf8b1984da04775debf276d82f
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-06-26 17:09:09 +0700

    Added Hero & Featured Posts Components

---
+++ components/home-page/featured-posts.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import classes from "./featured-posts.module.css";
+
+const FeaturedPosts = () => {
+  return (
+    <div className={classes.latest}>
+      <h2>Featured Posts</h2>
+    </div>
+  );
+};
+
+export default FeaturedPosts;

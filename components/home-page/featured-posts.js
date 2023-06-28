5b1edde165b1707b3bae148b79e73e8966bb3841
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-06-28 10:22:28 +0700

    Rendering Featured Posts

--- components/home-page/featured-posts.js
+++ components/home-page/featured-posts.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 import classes from "./featured-posts.module.css";
+import PostsGrid from "../posts/posts-grid";
 
-const FeaturedPosts = () => {
+const FeaturedPosts = (props) => {
   return (
-    <div className={classes.latest}>
+    <section className={classes.latest}>
       <h2>Featured Posts</h2>
-    </div>
+      <PostsGrid posts={props.posts} />
+    </section>
   );
 };
 

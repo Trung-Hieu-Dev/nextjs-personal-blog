668466824551bd69e911ebea25fa7ec115966131
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-06-29 10:45:03 +0700

    Added Head Data

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { Fragment } from "react";
 
+import Head from "next/head";
 import Hero from "../components/home-page/hero";
 import FeaturedPosts from "../components/home-page/featured-posts";
 
@@ -8,6 +9,13 @@ import { getFeaturedPosts } from "../lib/posts-utils";
 function HomePage(props) {
   return (
     <Fragment>
+      <Head>
+        <title>Hieu's Blog</title>
+        <meta
+          name="description"
+          content="I put my heart into creating this blog with the goal of finding a job."
+        />
+      </Head>
       <Hero />
       <FeaturedPosts posts={props.posts} />
     </Fragment>
